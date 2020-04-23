f133a4d549ceeec82a7f190c1e9397155da8c5d8
jheffh2012 <jheffh2012@gmail.com>
2020-04-23 19:18:52 -0300

    Melhorando visualizacao dos dados

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Camara Legislativa App');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Camara Legislativa App app is running!');
-  });
 });
--- src/app/core/pages/deputados/deputado-item/deputado-item.component.spec.ts
+++ src/app/core/pages/deputados/deputado-item/deputado-item.component.spec.ts
@@ -18,10 +18,20 @@ describe('DeputadoItemComponent', () => {
     fixture = TestBed.createComponent(DeputadoItemComponent);
     component = fixture.componentInstance;
     component.deputado = new DeputadoItem();
+    component.deputado.nome = "Deputado Teste";
+    component.deputado.siglaPartido = "PCOR";
+    component.deputado.email = "deputado@teste.com.br"
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render data of deputado', () => {
+    const compiled = fixture.debugElement.nativeElement;
+    console.log(compiled.querySelector('div h5'));
+    expect(compiled.querySelector('.deputado-nome').textContent).toContain('Deputado Teste');
+    expect(compiled.querySelector('.deputado-sigla').textContent).toContain('PCOR');
   });
 });
